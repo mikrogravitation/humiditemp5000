f0131af9a41b50efd754ea9c2a51993c52fe771e
Antonius Frie <antonius.frie@ruhr-uni-bochum.de>
2021-03-23 00:10:45 +0100

    generate config.py on the fly

--- deploy.py
+++ deploy.py
@@ -3,84 +3,66 @@ import json
 import binascii
 import hmac
 import hashlib
+import difflib
 import requests
+import git
 import re
+import yaml
 from sparkle import Sparkle
 from git import Repo
-
-device = sys.argv[1]
 
 
 def make_sparkle(glitter, data):
     return binascii.hexlify(Sparkle(glitter, data).make_sparkle())
 
 
-def get_remote_file_listing(remote):
-
-    print("trying to retrieve file listing...")
-
-    response = requests.get(f"http://{device}:5000/ota-listing")
-    assert response.status_code == 200
-
-    print("  done.")
-    print()
-
-    return parse_file_listing(response.text)
+sensor_types = {
+    "dht": {"port_type": "Pin"},
+    "bme": {"port_type": "I2C"},
+    "mhz": {"port_type": "UART"},
+    "sds": {"port_type": "UART"},
+    "counter": {"port_type": "Pin"},
+}
 
 
-def get_local_file_listing(repo):
+def get_sensor_config_string(sensor_name, sensor_config):
 
-    local_files = set()
+    sensor_type_info = sensor_types[sensor_config["type"]]
+    port_type = sensor_type_info["port_type"]
 
-    with open("deploy-listing") as f:
+    port_match = re.match(port_type + r"\(([0-9]+)\)", sensor_config["port"])
+    if not port_match:
+        raise RuntimeError(
+            f"Invalid port specification for sensor {sensor_name}: {sensor_config['port']} (sensor type {sensor_config['type']} needs port type {sensor_types[sensor_config['type']]['port_type']}"
+        )
 
-        for line in f:
-            filename = line.strip()
+    port_index = int(port_match.group(1))
 
-            # TODO: nicer error handling for when the file isn't there
-            # compute the git checksum for this file, plus, add it into
-            # git's object database
-            checksum = repo.git.hash_object(filename, w=True)
+    return f"""{{"type": "{sensor_config["type"]}", "port": machine.{port_type}({port_index}), "description": "{sensor_config["description"]}"}}"""
 
-            local_files.add((filename, checksum))
 
-    return local_files
+def make_config_py(device_name, device_info):
 
-def delete_remote_file(remote, glitter, filename):
+    sensor_configs = "sensor_configs = {\n"
+    for sensor_name, sensor_config in device_info["sensor_configs"].items():
+        sensor_configs += (
+            f'    "{sensor_name}": '
+            + get_sensor_config_string(sensor_name, sensor_config)
+            + ",\n"
+        )
+    sensor_configs += "}\n"
 
-    sparkle = make_sparkle(glitter, filename.encode("ascii"))
+    file_contents = (
+        f"""# this file is generated automatically from its corresponding entry in devices.yaml
+import machine
 
-    response = requests.delete(
-        f"http://{remote}:5000/ota/{filename}", params=dict(sparkle=sparkle)
+hostname = "{device_name}"
+
+"""
+        + sensor_configs
     )
 
-    print(
-        f"deleting file {filename}: {response.status_code} {response.reason}: {response.text}"
-    )
-
-    if response.status_code != 200:
-        raise RuntimeError("non-200 response code")
-
-
-def push_remote_file(remote, glitter, filename):
-
-    with open(filename, "rb") as f:
-        file_contents = f.read()
-
-    sparkle = make_sparkle(glitter, filename.encode("ascii") + b" " + file_contents)
-
-    response = requests.put(
-        f"http://{remote}:5000/ota/{filename}",
-        params=dict(sparkle=sparkle),
-        data=file_contents,
-    )
-
-    print(
-        f"pushing file {filename}: {response.status_code} {response.reason}: {response.text}"
-    )
-
-    if response.status_code != 200:
-        raise RuntimeError("non-200 response code")
+    return file_contents.encode("ascii")
 
 
 def parse_file_listing(raw_listing):
@@ -104,22 +86,107 @@ def parse_file_listing(raw_listing):
     return files
 
 
+def get_remote_file_listing(remote):
+
+    print("trying to retrieve file listing...")
+
+    response = requests.get(f"http://{device}:5000/ota-listing")
+    assert response.status_code == 200
+
+    print("  done.")
+    print()
+
+    return parse_file_listing(response.text)
+
+
+def git_blob_hash(data):
+
+    return hashlib.sha1(b"blob " + str(len(data)).encode("ascii") + b"\x00" + data).hexdigest()
+
+
+def get_local_file_listing(config_py):
+
+    local_files = set()
+    repo = git.Repo()
+
+    with open("deploy-listing") as f:
+
+        for line in f:
+            filename = line.strip()
+
+            if filename == "config.py":
+                # this file has been generated on-the-fly
+                checksum = git_blob_hash(config_py)
+
+            else:
+
+                # compute the git checksum for this file, plus, add it into
+                # git's object database
+                checksum = repo.git.hash_object(filename, w=True)
+
+            local_files.add((filename, checksum))
+
+    return local_files
+
+
+def delete_remote_file(remote, glitter, filename):
+
+    sparkle = make_sparkle(glitter, filename.encode("ascii"))
+
+    response = requests.delete(
+        f"http://{remote}:5000/ota/{filename}", params=dict(sparkle=sparkle)
+    )
+
+    print(
+        f"deleting file {filename}: {response.status_code} {response.reason}: {response.text}"
+    )
+
+    if response.status_code != 200:
+        raise RuntimeError("non-200 response code")
+
+
+def push_remote_file(remote, glitter, filename, file_contents=None):
+
+    if not file_contents:
+        with open(filename, "rb") as f:
+            file_contents = f.read()
+
+    sparkle = make_sparkle(glitter, filename.encode("ascii") + b" " + file_contents)
+
+    response = requests.put(
+        f"http://{remote}:5000/ota/{filename}",
+        params=dict(sparkle=sparkle),
+        data=file_contents,
+    )
+
+    print(
+        f"pushing file {filename}: {response.status_code} {response.reason}: {response.text}"
+    )
+
+    if response.status_code != 200:
+        raise RuntimeError("non-200 response code")
+
+
 if __name__ == "__main__":
 
-    with open("glitter.json") as f:
-        all_glitters = json.load(f)
+    device = sys.argv[1]
+
+    with open("devices.yaml") as f:
+        device_configs = yaml.safe_load(f)
 
     try:
-        glitter = all_glitters[device]
+        device_config = device_configs[device]
 
     except KeyError:
-        print(f"device {device} not found in glitterfile")
+        raise RuntimeError(f"device {device} not found in devices.yaml")
 
-    glitter = binascii.unhexlify(glitter)
+    glitter = binascii.unhexlify(device_config["glitter"])
+
+    config_py = make_config_py(device, device_config)
 
     # get file listings
     remote_files = get_remote_file_listing(device)
-    local_files = get_local_file_listing(Repo())
+    local_files = get_local_file_listing(config_py)
 
     remote_file_names = set(filename for filename, _ in remote_files)
     local_file_names = set(filename for filename, _ in local_files)
@@ -135,6 +202,10 @@ if __name__ == "__main__":
     print(f"files to push: {len(update_these_files)}")
     print()
 
+    #with open("config.py") as f:
+    #    contents = f.read()
+    #print("\n".join(difflib.unified_diff(contents.split("\n"), config_py.decode("ascii").split("\n"), fromfile="present", tofile="target")))
+
     for filename in delete_these_files:
         print(f"file {filename} will be removed.")
         delete_remote_file(device, glitter, filename)
@@ -147,7 +218,10 @@ if __name__ == "__main__":
         else:
             print(f"file {filename} is new.")
 
-        push_remote_file(device, glitter, filename)
+        if filename == "config.py":
+            push_remote_file(device, glitter, filename, file_contents=config_py)
+        else:
+            push_remote_file(device, glitter, filename)
         print()
 
     print("all good.")
